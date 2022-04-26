a6d79bf0408a0d9e7b30420491955983c7972e36
dp <donp@donp.org>
2022-04-26 12:55:49 -0700

    serde_json::from_value()

--- src/peer.rs
+++ src/peer.rs
@@ -24,8 +24,8 @@ pub fn read(db: Arc<crate::db::Db>, stream: TcpStream) {
     for line in peer.feed_lines() {
         let command: Command = serde_json::from_str(&line.unwrap()).unwrap();
         println!("{}", serde_json::to_string(&command).unwrap());
-        let loc : Location = serde_json::from_str(&command.noun.to_string()).unwrap();
-        println!("noun part: {} {} ", serde_json::to_string(&loc).unwrap(), &command.noun.to_string());
+        let loc : Location = serde_json::from_value(command.noun).unwrap();
+        println!("noun part: {}", serde_json::to_string(&loc).unwrap());
         let mut tx = db.env.begin_rw_txn().unwrap();
         let result = tx.get(db.db, &command.verb);
         match result {
