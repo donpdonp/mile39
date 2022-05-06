0a47c01e316c7d06e6cdb8bc3f80b8da309c96ad
dp <donp@donp.org>
2022-05-06 10:56:52 -0700

    read/write belong in db

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,12 @@
-use crate::schema;
+use lmdb::Cursor;
+use lmdb::DatabaseFlags;
 use lmdb::Environment;
+use lmdb::Transaction;
 use std::path::Path;
+
+use crate::nouns;
+use crate::nouns::*;
+use crate::schema;
 
 #[derive(Debug)]
 pub struct Db {
@@ -25,5 +31,61 @@ pub fn open() -> Db {
 impl Db {
     pub fn file_from_id(&self, id: &String) -> String {
         format!("{}/{}", self.file_path, id)
+    }
+
+    pub fn write(&self, noun: &Nouns) {
+        let value = serde_json::to_value(noun).unwrap();
+        let (noun_name, noun_value) = nouns::name_value(&value);
+        let schema = self.schemas.get(&noun_name);
+        if let Some(sch) = schema {
+            for index in sch.indexes.iter() {
+                let idx_db_name = self.schemas.db_name(&noun_name, &index.name);
+                let index_db = self
+                    .env
+                    .create_db(Some(&idx_db_name), DatabaseFlags::empty())
+                    .unwrap();
+                let mut tx = self.env.begin_rw_txn().unwrap();
+                let key = index.get_key(&noun_value);
+                let result = tx.get(index_db, &key);
+                match result {
+                    Err(_) => match noun {
+                        Nouns::Location(loc) => {
+                            println!(
+                                "writing {} key:{} value: {}",
+                                idx_db_name,
+                                String::from_utf8_lossy(&key),
+                                &loc.id
+                            );
+                            tx.put(index_db, &key, &loc.id, lmdb::WriteFlags::empty())
+                                .unwrap()
+                        }
+                    },
+                    Ok(v) => println!(
+                        "exists: {} {:?}: {:?}",
+                        idx_db_name,
+                        String::from_utf8_lossy(&key),
+                        String::from_utf8_lossy(v)
+                    ),
+                }
+                tx.commit().unwrap();
+                self.dump(&index.name);
+            }
+        }
+    }
+
+    pub fn dump(&self, name: &str) {
+        println!("---db dump {} ---", name);
+        let ddb = self.env.open_db(Some(&name)).unwrap();
+        let ro = self.env.begin_ro_txn().unwrap();
+        {
+            let mut c = ro.open_ro_cursor(ddb).unwrap();
+            let mut count = 0;
+            for kv in c.iter() {
+                count += count;
+                let k = String::from_utf8_lossy(kv.0);
+                let v = String::from_utf8_lossy(kv.1);
+                println!("{} {:?} {:?}", count, k, v);
+            }
+        }
     }
 }
--- src/peer.rs
+++ src/peer.rs
@@ -1,10 +1,6 @@
 use std::fs::File;
 use std::sync::Arc;
 
-use lmdb::Cursor;
-use lmdb::DatabaseFlags;
-use lmdb::Transaction;
-
 use serde_json;
 
 use crate::db;
@@ -49,70 +45,17 @@ impl Peer {
 pub fn read_op(db: &crate::db::Db, id: &String) -> PeerResult {
     let path = db.file_from_id(id);
     println!("read: {}", path);
-    let noun: nouns::Nouns =
-        serde_json::from_reader(File::open(db.file_from_id(&path)).unwrap()).unwrap();
+    let reader = File::open(path).unwrap();
+    let noun: nouns::Nouns = serde_json::from_reader(reader).unwrap();
     Ok(command::Response {
         msg: "ok".to_string(),
         noun: Some(noun),
     })
 }
-
 pub fn write_op(db: &crate::db::Db, noun: &Nouns) -> PeerResult {
-    let value = serde_json::to_value(noun).unwrap();
-    let (noun_name, noun_value) = nouns::name_value(&value);
-    let schema = db.schemas.get(&noun_name);
-    if let Some(sch) = schema {
-        for index in sch.indexes.iter() {
-            let idx_name = db.schemas.db_name(&noun_name, &index.name);
-            let index_db = db
-                .env
-                .create_db(Some(&idx_name), DatabaseFlags::empty())
-                .unwrap();
-            let mut tx = db.env.begin_rw_txn().unwrap();
-            let key = index.get_key(&noun_value);
-            println!("{} {}", idx_name, String::from_utf8_lossy(&key));
-            let result = tx.get(index_db, &key);
-            match result {
-                Err(_) => match noun {
-                    Nouns::Location(loc) => {
-                        println!(
-                            "writing {} key:{}",
-                            noun_name,
-                            String::from_utf8_lossy(&key)
-                        );
-                        tx.put(index_db, &key, &loc.id, lmdb::WriteFlags::empty())
-                            .unwrap()
-                    }
-                },
-                Ok(v) => println!(
-                    "{} {:?}: {:?}",
-                    index.name,
-                    String::from_utf8_lossy(&key),
-                    String::from_utf8_lossy(v)
-                ),
-            }
-            tx.commit().unwrap();
-            dump(&db, &index.name);
-        }
-    }
+    db.write(noun);
     Ok(command::Response {
         msg: "ok".to_string(),
         noun: None,
     })
-}
-
-pub fn dump(db: &crate::db::Db, name: &str) {
-    println!("---db dump {} ---", name);
-    let ddb = db.env.open_db(Some(&name)).unwrap();
-    let ro = db.env.begin_ro_txn().unwrap();
-    {
-        let mut c = ro.open_ro_cursor(ddb).unwrap();
-        let mut count = 0;
-        for kv in c.iter() {
-            count += count;
-            let k = String::from_utf8_lossy(kv.0);
-            let v = String::from_utf8_lossy(kv.1);
-            println!("{} {:?} {:?}", count, k, v);
-        }
-    }
 }
