512988d02c4abcbc664d6395c68c934a3965641d
dp <donp@donp.org>
2022-05-09 12:53:13 -0700

    test generate random locations

--- tests/main.rs
+++ tests/main.rs
@@ -9,17 +9,12 @@ fn write() {
     common::setup();
     let db = mile39::db::open();
     let peer = peer::new(Arc::new(db));
-    let location = location::Location {
-        id: common::id_generate(),
-        lat: 1.0,
-        lng: 2.0,
-        date: "2022-05-02".to_owned(),
-        user_id: "Abc".to_owned(),
-    };
+    let mut locations = common::random_locations(1);
+    let location = locations.pop().unwrap();
     let cmd = command::Command {
         verb: "write".to_owned(),
         noun: Some(Nouns::Location(location)),
-        id: None
+        id: None,
     };
     let json = serde_json::to_string(&cmd).unwrap();
     let result = peer.command(&json).unwrap();
@@ -28,7 +23,7 @@ fn write() {
     let cmd = command::Command {
         verb: "read".to_owned(),
         noun: None,
-        id: Some("ab13".to_owned())
+        id: Some("ab13".to_owned()),
     };
     let json = serde_json::to_string(&cmd).unwrap();
     let result = peer.command(&json).unwrap();
