611847d686662e5c790fa64322450295380aee4b
Don <donp@donp.org>
2022-05-17 10:57:00 -0700

    move out app bits

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 pub mod db;
 pub mod net;
 pub mod nouns;
-pub mod peer;
 pub mod pool;
 pub mod schema;
--- src/nouns.rs
+++ src/nouns.rs
@@ -1,7 +1,6 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
-pub mod command;
 pub mod location;
 
 #[derive(Serialize, Deserialize)]
